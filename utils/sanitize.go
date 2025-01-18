dc5ca86c4263c0a90f6007fe1902700aca1c6d62
plunder707 <125803170+plunder707@users.noreply.github.com>
2025-01-18 15:40:49 -0800

    Create sanitize.go

---
+++ utils/sanitize.go
@@ -0,0 +1,16 @@
+// utils/sanitize.go
+package utils
+
+import "strings"
+
+// SanitizeInput performs basic sanitization to prevent XSS.
+func SanitizeInput(input string) string {
+	replacer := strings.NewReplacer(
+		"<", "&lt;",
+		">", "&gt;",
+		"\"", "&quot;",
+		"'", "&#39;",
+		"&", "&amp;",
+	)
+	return replacer.Replace(input)
+}
