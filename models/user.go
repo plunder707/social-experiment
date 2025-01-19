95bcb49539d67a02fcfd2bcf8b8df1e5a172d96c
plunder707 <125803170+plunder707@users.noreply.github.com>
2025-01-19 08:35:32 -0800

    Update user.go

--- models/user.go
+++ models/user.go
@@ -1,12 +1,4 @@
 // models/user.go
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
-
-type Post struct {
-    ID        string `bson:"_id,omitempty" json:"id"`
-    UserID    string `bson:"user_id" json:"user_id"`
-    Username  string `bson:"username" json:"username"`
-    Content   string `bson:"content" json:"content"`
-    CreatedAt string `bson:"created_at" json:"created_at"`
-}
+// Removed the unused import and the duplicate Post struct
