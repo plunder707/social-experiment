41f64c4dc00784a54a243007a362002eabaad5db
plunder707 <125803170+plunder707@users.noreply.github.com>
2025-01-19 08:38:21 -0800

    Update user.go

--- models/user.go
+++ models/user.go
@@ -1,4 +1,11 @@
 // models/user.go
 package models
 
-// Removed the unused import and the duplicate Post struct
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type User struct {
+    ID        primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+    Username  string             `bson:"username" json:"username"`
+    Password  string             `bson:"password" json:"-"`
+    CreatedAt string             `bson:"created_at" json:"created_at"`
+}
