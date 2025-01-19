4b194f27e73ff9bb78d0869eed003af410cbe514
plunder707 <125803170+plunder707@users.noreply.github.com>
2025-01-18 16:03:44 -0800

    Create app.component.ts

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,9 @@
+// /frontend/src/app/app.component.ts
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `<app-navbar></app-navbar>
+    <router-outlet></router-outlet>`,
+})
+export class AppComponent {}
