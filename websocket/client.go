a09a205be78a63c9c6a583c7c9d854b62e519188
plunder707 <125803170+plunder707@users.noreply.github.com>
2025-01-19 08:55:26 -0800

    Update client.go

--- websocket/client.go
+++ websocket/client.go
@@ -9,34 +9,19 @@ import (
 
 // Client represents a WebSocket client
 type Client struct {
-    hub  *Hub
     conn *websocket.Conn
     send chan []byte
-}
-
-// NewClient creates a new WebSocket client
-func NewClient(hub *Hub, conn *websocket.Conn) *Client {
-    return &Client{
-        hub:  hub,
-        conn: conn,
-        send: make(chan []byte, 256),
-    }
 }
 
 // ReadPump listens for messages from the WebSocket connection
 func (c *Client) ReadPump() {
     defer func() {
-        c.hub.unregister <- c
         c.conn.Close()
     }()
     for {
         _, _, err := c.conn.ReadMessage()
         if err != nil {
-            if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
-                log.Printf("[ERROR] WebSocket closed unexpectedly: %v", err)
-            } else {
-                log.Printf("[INFO] WebSocket closed: %v", err)
-            }
+            log.Printf("[INFO] WebSocket closed: %v", err)
             break
         }
         // Handle incoming messages if necessary
@@ -52,10 +37,7 @@ func (c *Client) WritePump() {
         message, ok := <-c.send
         if !ok {
             // Hub closed the channel
-            err := c.conn.WriteMessage(websocket.CloseMessage, []byte{})
-            if err != nil {
-                log.Printf("[ERROR] WebSocket close message error: %v", err)
-            }
+            c.conn.WriteMessage(websocket.CloseMessage, []byte{})
             return
         }
         err := c.conn.WriteMessage(websocket.TextMessage, message)
